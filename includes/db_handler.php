156776d21f7dc15617ae2ad805b829559c8e2b7c
MateuszHermann <71326357+mateuszhermann@users.noreply.github.com>
2021-07-21 23:16:13 +0200

    Update db_handler.php

--- includes/db_handler.php
+++ includes/db_handler.php
@@ -1,10 +1,10 @@
 <?php
-
+#ze względu na dane logownaia do bazy parametry zastąpiłem argumenty placeholderem 
 $connection=mysqli_connect(
-    "serwer2038562.home.pl",
-    "33358098_hermann1",
-    "jmjmjmjm2137@",
-    "33358098_hermann1"
+    #"Placeholder",
+    #"placeholder",
+    #"placeholder",
+    #"placeholder"
 );
 if(mysqli_connect_errno($connection)!=0){
     print 'error';
